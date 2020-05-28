d4d3ef5360a2988b70cfdd8b55121acc265e5a21
Crystal Ho <cth6xmj@virginia.edu>
2020-05-27 21:33:57 -0400

    Created a disappearing and appearing header based on scroll

--- scriptGeneral.js
+++ scriptGeneral.js
@@ -1,11 +1,14 @@
 
+// DISAPEARING HEADER
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
 var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("header_container").style.top = "0";
   } else {
-    document.getElementById("header_container").style.top = "-50px";
+    document.getElementById("header_container").style.top = "-1024px";
   }
   prevScrollpos = currentScrollPos;
 }
+
+
