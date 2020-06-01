578ca2f1e91e41fd97d25fbf892270ab7106f46b
J4102 <JYang8501@gmail.com>
2020-06-01 18:38:50 -0400

    Add ability to add text to stack items

--- animations.js
+++ animations.js
@@ -10,7 +10,7 @@ canvas.height = 800;
 
 
 //x & y == top left corner of stack
-function Stack(x, y, itemWidth, itemHeight, numItems, speed)
+function Stack(x, y, itemWidth, itemHeight, numItems, speed, data)
 {   
     //Current values - changes while items are being removed and added
     this.x = x;
@@ -22,6 +22,8 @@ function Stack(x, y, itemWidth, itemHeight, numItems, speed)
 
     c.fillStyle = "#ABCD05";
     c.strokeStyle="#000000";
+
+    
     
     //Current stack item value (remove/add)
     this.currX = this.x;
@@ -45,10 +47,10 @@ function Stack(x, y, itemWidth, itemHeight, numItems, speed)
             c.fillRect(this.x, newY, this.itemWidth, this.itemHeight);
             c.strokeRect(this.x, newY, this.itemWidth, this.itemHeight);
             
-
+            c.fillStyle = "#000000";
             //Draw text on rectangle   
-            //c.fillText("1", this.x, this.y, this.itemWidth, this.itemHeight);
-
+            c.fillText(data[i], this.x + (this.x/2), newY + (this.itemHeight/2), this.itemWidth, this.itemHeight);
+            c.fillStyle = "#ABCD05";
         }
     }
 
@@ -67,77 +69,16 @@ function Stack(x, y, itemWidth, itemHeight, numItems, speed)
         snd.play();
     }
 
-    this.push = function()
+    this.push = function(num)
     {
         this.numItems++;
         this.y-=this.itemHeight;
+        data.unshift(num);
 
         this.draw();
 
         var snd = new Audio("sounds/pop.flac");
         snd.play();
-    }
-
-    /*
-    -------------------------------------------------------------------------------------------
-    -------------------------------------------------------------------------------------------
-    ------------------------NON INTERACTIVE MOVING THE RECTANGLES -----------------------------
-    -------------------------------------------------------------------------------------------
-    -------------------------------------------------------------------------------------------
-    */
-
-
-
-    /*
-    Remove item functions  
-
-    1. Remove the item
-    2. Move to the right
-    3. Move it down
-    */
-    this.removeTopItem = function()
-    {
-        if(numItems == 0)
-            console.log("No items to remove!");
-
-        //Animate removing item here
-        c.fillStyle = "#FFFFFF";
-        c.strokeStyle = "#FFFFFF";
-        c.fillRect(this.x, this.y, this.itemWidth, this.itemHeight);
-        c.strokeRect(this.x, this.y, this.itemWidth, this.itemHeight);
-
-        //Refreshes colors
-        c.fillStyle = "#ABCD05";
-        c.strokeStyle="#000000";
-        
-        //New stack height
-        this.y+=this.itemHeight;
-        
-        this.numItems = this.numItems-1;
-
-    }
-
-
-    //Moves the top stack item to the right
-    this.moveRight = function()
-    {
-        this.currX+=this.speed;
-        c.beginPath();
-        c.fillRect(this.currX, this.currY, this.itemWidth, this.itemHeight);
-        c.strokeRect(this.currX, this.currY, this.itemWidth, this.itemHeight);
-
-        this.draw();
-    }
-
-    //Moves the top stack item down
-    this.moveDown = function()
-    {
-        this.currY+=this.speed;
-        c.beginPath();
-        c.fillRect(this.currX, this.currY, this.itemWidth, this.itemHeight);
-        c.strokeRect(this.currX, this.currY, this.itemWidth, this.itemHeight);
-
-        this.draw();
     }
 
     this.reset = function()
@@ -185,12 +126,12 @@ function Stack(x, y, itemWidth, itemHeight, numItems, speed)
 /*
     -------------------------------------------------------------------------------------------
     -------------------------------------------------------------------------------------------
-    ------------------------ Initializing object -----------------------------
+    ------------------------ Initializing object ----------------------------------------------
     -------------------------------------------------------------------------------------------
     -------------------------------------------------------------------------------------------
-    */
+*/
 
-var stack = new Stack(100, 100, 100, 50, 3, 2);
+var stack = new Stack(100, 100, 100, 100, 3, 2, [1,2,3]);
 stack.draw();
 
 //These will determine which animations will be playing
@@ -210,40 +151,9 @@ function animate()
 
     //Refresh screen
     c.clearRect(0, 0, window.innerWidth, window.innerHeight);
+
+    //Repeatedly draw the rectangle on the screen
     stack.draw();
-
-    // if(pop)
-    // {
-        //Without sound - removing the stack item
-
-        // if(!removed)
-        // {
-        //     stack.removeTopItem();
-        //     removed = true;
-        // }
-        // else if(right < stack.getItemWidth())
-        // {
-        //     stack.moveRight();
-        //     right+=stack.getSpeed();
-        // }
-        // else if(down < stack.getHeight())    
-        // {
-        //     stack.moveDown();
-        //     down+=stack.getSpeed();
-        // }
-        // //Repeat the animation again
-        // else
-        // {
-        //     stack.reset();
-        //     right = 0;
-        //     down = 0;
-        //     removed = false;
-
-        //     //Wait 5 seconds here
-        // }
-        //}
-        
-
 
 }
 
@@ -261,7 +171,7 @@ document.getElementById("pushBtn").addEventListener("click",
 
     function()
     {
-        stack.push();
+        stack.push("yoyo");
     }
 );
 
